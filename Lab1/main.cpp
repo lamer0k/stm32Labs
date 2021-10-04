0c3295e2e9bc29a79193c59f9e111596791a8126
lamer0k <sergey.kolody@gmail.com>
2021-10-04 21:13:38 +0500

    lab1

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,4 +1,6 @@
 #include "rccregisters.hpp" // for RCC
+#include "gpioaregisters.hpp" // for GPIOA
+#include "gpiocregisters.hpp" // for GPIOС
 
 std::uint32_t SystemCoreClock = 16'000'000U;
 
@@ -18,16 +20,43 @@ int __low_level_init(void)
 
   }
   
-  RCC::APB2ENR::SYSCFGEN::Enable::Set(); 
+  RCC::APB2ENR::SYSCFGEN::Enable::Set();
 
   return 1;
 }
 }
 
+void delay(int cycles)
+{
+  for(int i = 0; i < cycles; ++i)    
+  {   
+    asm volatile("");
+  }    
+}
 
 int main()
-{
- 
-
-  return 0;
+{  
+  //Подать тактирование на порт А
+  RCC::AHB1ENR::GPIOAEN::Enable::Set() ;
+  //Подать тактирование на порт С
+  RCC::AHB1ENR::GPIOCEN::Enable::Set() ;
+  //Порта А.5 на вывод
+  GPIOA::MODER::MODER5::Output::Set() ;
+  //Порта C.5,C.8, C.9 на вывод
+  GPIOC::MODER::MODER5::Output::Set() ;
+  GPIOC::MODER::MODER8::Output::Set() ;
+  GPIOC::MODER::MODER9::Output::Set() ;
+  
+  for(;;)
+  {
+   
+     GPIOA::ODR::ODR5::High::Set();
+     GPIOC::ODR::ODR5::High::Set();
+     delay(1000000); 
+     GPIOA::ODR::ODR5::Low::Set();
+     GPIOC::ODR::ODR5::Low::Set();
+     delay(1000000); 
+  }
+  
+  return 1;
 }
